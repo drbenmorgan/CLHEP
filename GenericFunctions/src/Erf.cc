811f80700c03072fc12e5a107bc2c6580fa355ef
Lynn Garren <garren@fnal.gov>
2003-11-21 16:48:14 +0000

    cmath in CLHEP_2_000_branch

--- GenericFunctions/src/Erf.cc
+++ GenericFunctions/src/Erf.cc
@@ -1,5 +1,7 @@
 // -*- C++ -*-
-// $Id: Erf.cc,v 1.4 2003/09/06 14:04:14 boudreau Exp $
+// $Id: Erf.cc,v 1.4.4.1 2003/11/21 16:48:14 garren Exp $
+
+#include <cmath>
 
 #include "CLHEP/GenericFunctions/Erf.hh"
 #include "CLHEP/GenericFunctions/Gaussian.hh"
