ef85ba2d2dd76e0d8600a008b9998dbaf7cd1fe0
Lynn Garren <garren@fnal.gov>
2004-06-23 23:37:21 +0000

    version is now 1.01.00

--- HepPDT/src/version.cc
+++ HepPDT/src/version.cc
@@ -1,4 +1,4 @@
-// $Id: version.cc,v 1.1.1.1 2003/07/15 20:15:05 garren Exp $
+// $Id: version.cc,v 1.1.1.1.2.1 2004/06/23 23:37:21 garren Exp $
 // ----------------------------------------------------------------------
 //
 // version.cc
@@ -15,7 +15,7 @@ namespace HepPDT {
 
 std::string versionName( )
 {
-    return "1.00.01";
+    return "1.01.00";
 }
 
 void version( )
