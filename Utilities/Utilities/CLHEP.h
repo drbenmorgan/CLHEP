e75f419873f368ad25f1e124811eb7f659205b55
Lynn Garren <garren@fnal.gov>
2004-07-23 20:23:01 +0000

    remove obsolete Utilities from CLHEP 2.0 to avoid confusion

--- Utilities/Utilities/CLHEP.h
+++
@@ -1,6 +0,0 @@
-// $Id: CLHEP.h,v 1.1.1.1 2003/07/15 20:15:05 garren Exp $
-// ----------------------------------------------------------
-// For backward compatibility
-//
-#include "CLHEP/config/CLHEP.h"
-#include "CLHEP/config/TemplateFunctions.h"
