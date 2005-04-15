458cb06fd7da96d8f9ccf52fd97bafbe8069311b
Lynn Garren <garren@fnal.gov>
2005-04-15 20:49:38 +0000

    merging changes from 2.0 branch into head

--- Cast/src/itos.cc
+++ Cast/src/itos.cc
@@ -10,12 +10,7 @@
 // ----------------------------------------------------------------------
 
 
-#include "CLHEP/Cast/defs.h"
-#if HAVE_SSTREAM
 #include <sstream>
-#else
-#include "CLHEP/Cast/StringStream.h"
-#endif
 #include "CLHEP/Cast/itos.h"
 
 
