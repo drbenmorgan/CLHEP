35df634df3bd075d9e9324d32be36b512f04fcb1
Lynn Garren <garren@fnal.gov>
2006-05-17 21:59:36 +0000

    merging in changes from 2.0

--- HepMC/src/GenEvent.cc
+++ HepMC/src/GenEvent.cc
@@ -12,7 +12,6 @@
 
 #include "CLHEP/HepMC/GenEvent.h"
 #include "CLHEP/HepMC/DoubleConversion.hh"
-#include <stdio.h>       // needed for formatted output using sprintf 
 
 namespace HepMC {
 
@@ -137,11 +136,10 @@ namespace HepMC {
 	     << " [energy] \t alphaQCD=" << alphaQCD() 
 	     << "\t alphaQED=" << alphaQED() << std::endl;
 	// print a legend to describe the particle info
-	char particle_legend[80];
-	sprintf( particle_legend,"      %9s %8s %9s,%9s,%9s,%8s %4s %9s",
-		 "Barcode","PDG ID","( Px","Py","Pz","E )","Stat","DecayVtx");
-	ostr << "                                    GenParticle Legend\n"
-	     << particle_legend << "\n";
+	ostr << "                                    GenParticle Legend\n";
+ 	ostr  << "        Barcode   PDG ID      "
+	      << "( Px,       Py,       Pz,     E )"
+	      << " Stat  DecayVtx\n";
 	ostr << "________________________________________"
 	     << "________________________________________\n";
 	// Print all Vertices
