9170d2c1d780724624f93bea38f7d714b221925d
Lynn Garren <garren@fnal.gov>
2009-11-11 23:18:26 +0000

    save the change to ThreeVector for 2.0.5.0

--- Vector/Vector/ThreeVector.h
+++ Vector/Vector/ThreeVector.h
@@ -1,6 +1,6 @@
 // -*- C++ -*-
 // CLASSDOC OFF
-// $Id: ThreeVector.h,v 1.2.4.1.2.1 2009/11/03 21:35:19 garren Exp $
+// $Id: ThreeVector.h,v 1.2.4.1.2.2 2009/11/11 23:18:26 garren Exp $
 // ---------------------------------------------------------------------------
 // CLASSDOC ON
 //
@@ -48,10 +48,7 @@ public:
   // Safe indexing of the coordinates when using with matrices, arrays, etc.
   // (BaBar)
 
-  Hep3Vector();
-  explicit Hep3Vector(double x);
-  Hep3Vector(double x, double y);
-  Hep3Vector(double x, double y, double z);
+  inline Hep3Vector(double x = 0.0, double y = 0.0, double z = 0.0);
   // The constructor.  
 
   inline Hep3Vector(const Hep3Vector &);
