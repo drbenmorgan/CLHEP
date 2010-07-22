376196fe7d3d8cc83400606db4d81f44ba949510
Lynn Garren <garren@fnal.gov>
2010-07-22 21:55:10 +0000

    fix

--- GenericFunctions/src/AnalyticConvolution.cc
+++ GenericFunctions/src/AnalyticConvolution.cc
@@ -1,10 +1,10 @@
 // -*- C++ -*-
-// $Id: AnalyticConvolution.cc,v 1.7 2010/07/22 21:50:53 garren Exp $
+// $Id: AnalyticConvolution.cc,v 1.8 2010/07/22 21:55:10 garren Exp $
 #include "CLHEP/GenericFunctions/AnalyticConvolution.hh"
 #include "CLHEP/GenericFunctions/Gaussian.hh"
 #include "CLHEP/GenericFunctions/Exponential.hh"
 #include <cmath>	// for isfinite
-#if (defined __STRICT_ANSI__) || (defined _WIN32)
+#if (defined _WIN32)
 #include <float.h> //  Visual C++ _finite
 #endif
 namespace Genfun {
@@ -77,7 +77,7 @@ double AnalyticConvolution::operator() (double argument) const {
   if (_type==SMEARED_NEG_EXP) {
     expG = exp((sigma*sigma +2*tau*(/*offset*/x))/(2.0*tau*tau)) * 
       erfc((sigma*sigma+tau*(/*offset*/x))/(sqrtTwo*sigma*tau))/(2.0*tau);
-#if (defined __STRICT_ANSI__) || (defined _WIN32)
+#if (defined _WIN32)
     if (!_finite(expG)) {
       expG=0.0;
     }
@@ -95,7 +95,7 @@ double AnalyticConvolution::operator() (double argument) const {
   
   // Both sign distribution=> return smeared exponential:
   if (_type==SMEARED_EXP) {
-#if (defined __STRICT_ANSI__) || (defined _WIN32)
+#if (defined _WIN32)
     if (!_finite(expG)) {
       expG=0.0;
     }
