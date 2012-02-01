6b89efc3879a45e39381edb5b183ea53ea4475ad
Lynn Garren <garren@fnal.gov>
2012-02-01 23:20:38 +0000

    get rid of testBug6740.sh

--- Geometry/test/testBug6740.cc
+++ Geometry/test/testBug6740.cc
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <cmath>
+#include <float.h>
 
 #include "CLHEP/Geometry/Point3D.h"
 #include "CLHEP/Vector/ThreeVector.h"
@@ -11,9 +13,20 @@ int main(int, char **) {
   CLHEP::Hep3Vector d(10,0,0);
   HepPoint3D a(0,0,0);
   HepPoint3D pv(1,1,1);
+  
+  float expected = 1.41421;
+  int numbad = 0;
 
   CLHEP::Hep3Vector dist = a-pv; // this does not compile anymore 
   CLHEP::Hep3Vector Cross = dist.cross(d.unit()); 
   std::cout << Cross.mag() << std::endl;
+  
+  float result = Cross.mag();
+  float eps = 1.0e-5;
+  if( fabs(result - expected) >  eps ) {
+    ++numbad;
+    std::cout << "Expected " << expected << " got " << result << std::endl;
+  }
+  return numbad;
 
 }
