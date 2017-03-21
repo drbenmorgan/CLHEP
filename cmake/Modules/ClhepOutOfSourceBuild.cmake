a22d5c4b35083c9fd3e727275bced0be57a785e2
Lynn Garren <garren@fnal.gov>
2017-03-21 13:57:48 -0500

    fix CLHEP-137

--- cmake/Modules/ClhepOutOfSourceBuild.cmake
+++ cmake/Modules/ClhepOutOfSourceBuild.cmake
@@ -2,10 +2,17 @@
 # clhep_ensure_out_of_source_build()
 #
 
+# we need the full path here because we check for an out of source build before anything else
+include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Modules/ClhepRegexEscape.cmake)
+
 macro (clhep_ensure_out_of_source_build)
 
   string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" in_source)
-  string( REGEX MATCH "${CMAKE_SOURCE_DIR}/" in_source_subdir "${CMAKE_BINARY_DIR}")
+  clhep_regex_escape( "${CMAKE_SOURCE_DIR}" escaped_path)
+  set(my_source_dir "^${escaped_path}$")
+  clhep_regex_escape( "${CMAKE_BINARY_DIR}" escaped_path)
+  set(my_binary_dir "^${escaped_path}$")
+  string( REGEX MATCH "${my_source_dir}/" in_source_subdir "${my_binary_dir}")
   if (in_source OR in_source_subdir)
   message(FATAL_ERROR "
 ERROR: In source builds of this project are not allowed.
