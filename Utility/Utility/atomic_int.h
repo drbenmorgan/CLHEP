605fcdddf709ee46f403e3a9105409aaf5cf8756
Lynn Garren <garren@fnal.gov>
2018-03-15 15:00:55 -0500

    modern clang compilers without atomic fail the __cplusplus >= 201103L test

--- Utility/Utility/atomic_int.h
+++ Utility/Utility/atomic_int.h
@@ -10,16 +10,9 @@
 
 #if __cplusplus >= 201103L
 
-  #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 7) || USING_VISUAL
+  #if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ > 7) || __clang__ || USING_VISUAL
     #include <atomic>
     #define CLHEP_ATOMIC_INT_TYPE std::atomic<int>
-  #elif __clang__
-    #if __has_extension(c_atomic)
-      #include <atomic>
-      #define CLHEP_ATOMIC_INT_TYPE std::atomic<int>
-    #else
-      #define CLHEP_ATOMIC_INT_TYPE int
-    #endif
   #else
     #define CLHEP_ATOMIC_INT_TYPE int
   #endif
