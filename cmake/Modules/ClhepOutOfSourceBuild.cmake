3d449b27cae1efecae1bf01115c58ce99a7c498c
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2022-01-21 12:37:49 +0000

    Simplify out-of-source check
    Current regex based system very sensitive to path contents. Simply
    check for path equality directly in conditional. Check for subdirs
    by checking for the build directory string containing the source
    directory as a substring starting at index 0.

--- cmake/Modules/ClhepOutOfSourceBuild.cmake
+++ cmake/Modules/ClhepOutOfSourceBuild.cmake
@@ -1,22 +1,15 @@
-# Throw a fatal error if cmake is invoked from within the source code directory tree
+# Throw a fatal error if cmake is invoked from the source code directory tree
 # clhep_ensure_out_of_source_build()
 #
 
-# we need the full path here because we check for an out of source build before anything else
-include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Modules/ClhepRegexEscape.cmake)
-
 macro (clhep_ensure_out_of_source_build)
+  string(FIND "${CMAKE_BINARY_DIR}" "${CMAKE_SOURCE_DIR}" is_subdir_pos)
 
-  string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" in_source)
-  clhep_regex_escape( "${CMAKE_SOURCE_DIR}" escaped_path)
-  set(my_source_dir "^${escaped_path}$")
-  clhep_regex_escape( "${CMAKE_BINARY_DIR}" escaped_path)
-  set(my_binary_dir "^${escaped_path}$")
-  string( REGEX MATCH "${my_source_dir}/" in_source_subdir "${my_binary_dir}")
-  if (in_source OR in_source_subdir)
+  if ((CMAKE_BINARY_DIR STREQUAL CMAKE_SOURCE_DIR)
+      OR (is_subdir_pos EQUAL 0))
   message(FATAL_ERROR "
 ERROR: In source builds of this project are not allowed.
-A separate build directory is required.
+A separate build directory outside of `${CMAKE_SOURCE_DIR}' is required.
 Please create one and run cmake from the build directory.
 Also note that cmake has just added files to your source code directory.
 We suggest getting a new copy of the source code.
@@ -24,4 +17,4 @@ Otherwise, delete `CMakeCache.txt' and the directory `CMakeFiles'.
   ")
   endif ()
 
-endmacro (clhep_ensure_out_of_source_build)
+endmacro ()
